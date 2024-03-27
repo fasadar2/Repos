941e3a5bb1cf817ee4b6812399e632d98ce2952b
oma <oma@pbprog.ru>
2024-03-27 18:51:20 +0300

    соме чанджес то рефактор тактор зис плиз комит ченджес эмидиатли асаб

--- repository.py
+++ repository.py
@@ -1,14 +1,18 @@
-#Я бы тебе советовал все равно делать на классах ибо это нормально блеать
+# Я бы тебе советовал все равно делать на классах ибо это нормально блеать
 class Repository:
     def __init__(self):
-        self._employee:dict = {}
+        self._employee: dict = {}
+
     def get_repos(self):
         return self._employee
-    def update_repos(self,**kwargs):
-        update =  {kwargs['id']:{"fio":kwargs['fio'],"position":kwargs['position']}}
-        #Если записи с таким id нет, то добавляем
+
+    def update_repos(self, **kwargs):
+        update = {kwargs['id']: {"fio": kwargs['fio'], "position": kwargs['position']}}
+        # Если записи с таким id нет, то добавляем
         self._employee.update(update)
-    def get_attr(self,id,attr:str):
+
+    def get_attr(self, id, attr: str):
         return self._employee[id][attr]
-    def set_attr(self,id,attr:str,value):
+
+    def set_attr(self, id, attr: str, value):
         self._employee[id][attr] = value
