4e025b8656833bf7246b23e460c5a20d378c997e
oma <oma@pbprog.ru>
2024-03-27 18:41:41 +0300

    соме чанджес то рефактор тактор зис

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from repository import Repository
-#Инициализируем репозиторий с пиздосиком
+
+# Инициализируем репозиторий с пиздосиком
 repo = Repository()
 id = 1
 fio = "булььбазавр бульбазаврович"
 position = "пиздосик"
-repo.update_repos(id=id,fio=fio,position=position)
-idIn =int(input("введите id: "))
+
+# Добавляем сотрудника
+repo.update_repos(id=id, fio=fio, position=position)
+
+# Просим id для дальнейшей демонстрации
+idIn = int(input("введите id: "))
+
+# демонстрируем работу репозитрия
 print(repo.get_repos())
-print(repo.get_attr(id=idIn,attr='fio'))
-repo.set_attr(id=idIn,attr='position',value='Мажажаер')
+
+# вывдем по id только fio
+print(repo.get_attr(id=idIn, attr='fio'))
+
+# повысим пиздосика до Мажажаера
+repo.set_attr(id=idIn, attr='position', value='Мажажаер')
+
+# Убедимся что пиздосик повышен
 print(repo.get_repos())
